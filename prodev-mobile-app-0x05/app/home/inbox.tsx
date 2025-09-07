1d8b28fc49af0654712f0761a1681e6e360f1d98
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-09-07 13:10:52 +0300

    feat(home): add Inbox screen placeholder

---
+++ prodev-mobile-app-0x05/app/home/inbox.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+
+const Inbox = () => {
+    return (
+        <View>
+            <Text>Inbox Screen</Text>
+        </View>
+    )
+}
+
+export default Inbox;
